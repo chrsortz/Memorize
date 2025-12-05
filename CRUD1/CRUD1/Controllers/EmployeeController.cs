2e5b25f7c977fc49578c128204c943d953ec9857
Christian Dave A. Ortiz <christianortiz.dave@gmail.com>
2025-12-05 12:25:35 -0800

    - making Create controller and razor view

--- CRUD1/CRUD1/Controllers/EmployeeController.cs
+++ CRUD1/CRUD1/Controllers/EmployeeController.cs
@@ -2,6 +2,8 @@
 using CRUD1.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Reflection.Metadata.Ecma335;
 
 namespace CRUD1.Controllers
 {
@@ -13,9 +15,29 @@ namespace CRUD1.Controllers
         {
             _context = context;
         }
+
+        //Dashboard - Index
        public async Task<IActionResult> Index()
         {
             return View(await _context.Employees.ToListAsync());
         }
+
+        //Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Employees.Add(employee);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }       
     }
 }
